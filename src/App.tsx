75107ad071e5de7eb99dcae62ba1bff8fd7f7639
seoAreum <1971236@hansung.ac.kr>
2023-11-18 01:39:51 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <RecoilRoot>
       {/* ThemeProvider, GlobalStyle, Router 추가해야 함 ! */}
-      <>초기세팅어어어아아아 디벱롭 푸시</>
+      <>초기세팅어어어아아아 디벱롭 푸시 어어</>
     </RecoilRoot>
   );
 }
