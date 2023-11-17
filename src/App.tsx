206c9c2b0c359cdb87305e2d59ba7e85a7974465
seoAreum <1971236@hansung.ac.kr>
2023-11-17 16:36:33 +0900

    develop initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <RecoilRoot>
       {/* ThemeProvider, GlobalStyle, Router 추가해야 함 ! */}
-      <>초기세팅어어어</>
+      <>초기세팅어어어아아아 디벱롭 푸시</>
     </RecoilRoot>
   );
 }
