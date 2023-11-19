2b5394d03c8d99ed6c26419b54dc87d3eb38104d
Minseo Kang <rachel5640@naver.com>
2023-11-20 03:43:36 +0900

    style: globalstyle 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { RecoilRoot } from "recoil";
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import styled, { css } from 'styled-components';
+import './styles/fonts/fonts.css';
 
 function App() {
+  console.log('초기세팅 완료');
+
   return (
     <RecoilRoot>
-      {/* ThemeProvider, GlobalStyle, Router 추가해야 함 ! */}
-      <>초기세팅어어어아아아 디벱롭 푸시 어어</>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {/* Router 추가해야 함 !  */}
+        <BlueHeading>적용 잘됨 확인!</BlueHeading>
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
+
+const BlueHeading = styled.h1`
+  ${({ theme }) => css`
+    color: ${theme.colors.pointColor.blue0};
+    ${theme.fonts.heading1};
+  `}
+`;
 
 export default App;
