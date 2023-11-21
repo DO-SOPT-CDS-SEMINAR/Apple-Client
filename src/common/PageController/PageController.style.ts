e54edbbc772fe91409ef047fac70097ef4e93e6c
Wang HoEun <hoeunwang@gmail.com>
2023-11-22 00:12:22 +0900

    style: 페이지 tsx로 수정 및 구조 잡기 및 style 구현

---
+++ src/common/PageController/PageController.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 40rem;
+  height: 2.7rem;
+  gap: 1rem;
+`;
---
+++ src/common/PageController/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './PageController.style';
+
+export const PageController = () => {
+  return <S.Wrapper>아이콘 넣어주기</S.Wrapper>;
+};
