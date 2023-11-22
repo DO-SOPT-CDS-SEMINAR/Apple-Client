679aa96883ff14a08796bb3374db215267d13bd7
Wang HoEun <hoeunwang@gmail.com>
2023-11-22 17:10:46 +0900

    style: app 파일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import styled, { css } from 'styled-components';
 import './styles/fonts/fonts.css';
-import { BuyButton } from './common/Button/BuyButton';
-import { DefaultButton } from './common/Button/DefaultButton';
-import { DisplayTypeButton } from './common/Button/DisplayTypeButton';
-import { GigaTypeButton } from './common/Button/GigaTypeButton';
-import { AddToCartButton } from './common/Button/AddToCartButton';
-import { PlusIconButton } from './common/Button/PlusIconButton';
-import { ColorTypeButton } from './common/Button/ColorTypeButton';
-import { IcSwatchSpaceGray } from './assets/icon';
 
 function App() {
   console.log('초기세팅 완료');
@@ -22,13 +14,6 @@ function App() {
         <GlobalStyle />
         {/* Router 추가해야 함 !  */}
         <BlueHeading>적용 잘됨 확인!</BlueHeading>
-        <BuyButton>구입하기</BuyButton>
-        <DefaultButton>button</DefaultButton>
-        <DisplayTypeButton title='Title' subtext='subtext' price={1729000} disabled />
-        <GigaTypeButton giga={128} price={1729000} disabled />
-        <AddToCartButton>장바구니에 담기</AddToCartButton>
-        <PlusIconButton>Title</PlusIconButton>
-        <ColorTypeButton ColorComponent={IcSwatchSpaceGray}>스페이스 그레이</ColorTypeButton>
       </ThemeProvider>
     </RecoilRoot>
   );
