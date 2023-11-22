6def24254f9e57d703b55b69cf04bca3185d34c4
Wang HoEun <hoeunwang@gmail.com>
2023-11-22 17:13:41 +0900

    style: app파일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import styled, { css } from 'styled-components';
 import './styles/fonts/fonts.css';
-import { PageController } from './common/PageController';
 
 function App() {
   console.log('초기세팅 완료');
@@ -15,7 +14,6 @@ function App() {
         <GlobalStyle />
         {/* Router 추가해야 함 !  */}
         <BlueHeading>적용 잘됨 확인!</BlueHeading>
-        <PageController pages={5} nowPage={2} />
       </ThemeProvider>
     </RecoilRoot>
   );
