1ff07f74cfa1b314870977189e7286869c5b248f
Wang HoEun <hoeunwang@gmail.com>
2023-11-22 17:12:33 +0900

    style: app 파일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,9 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import styled, { css } from 'styled-components';
 import './styles/fonts/fonts.css';
-import { Dropdown } from './components/Dropdown';
 
 function App() {
   console.log('초기세팅 완료');
-  const handleSelectChange = (combinedValue: string) => {
-    console.log(combinedValue);
-  };
 
   return (
     <RecoilRoot>
@@ -18,7 +14,6 @@ function App() {
         <GlobalStyle />
         {/* Router 추가해야 함 !  */}
         <BlueHeading>적용 잘됨 확인!</BlueHeading>
-        <Dropdown onSelection={handleSelectChange} />
       </ThemeProvider>
     </RecoilRoot>
   );
