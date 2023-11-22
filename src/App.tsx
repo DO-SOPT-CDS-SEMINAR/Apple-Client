20573654e400b3d879ea0aad1e3636cd67428560
Minseo Kang <rachel5640@naver.com>
2023-11-22 14:56:50 +0900

    remove: App에서 불필요한 import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import './styles/fonts/fonts.css';
-import Gnb from './common/Gnb/index';
-import Banner from './common/Banner';
-import Nb from './common/Nb';
 
 function App() {
   console.log('초기세팅 완료');
