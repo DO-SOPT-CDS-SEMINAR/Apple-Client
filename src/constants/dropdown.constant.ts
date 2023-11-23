4443c2db3a9ea3fb98f76dbe8e5a13c473288ce9
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2023-11-23 10:41:39 +0900

    [Common] Dropdown component 구현 (#19)
    * feat: Dropdown component 생성
    
    * feat: dropdown icon 추가
    
    * style: app 파일 초기화
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

---
+++ src/constants/dropdown.constant.ts
@@ -0,0 +1,116 @@
+export const DropDownNow = [
+  {
+    key: 0,
+    value: 'iPad Pro 12.9(6세대)',
+  },
+  {
+    key: 1,
+    value: 'iPad Pro 11(4세대)',
+  },
+  {
+    key: 2,
+    value: 'iPad(10세대)',
+  },
+  {
+    key: 3,
+    value: 'iPad(9세대)',
+  },
+  {
+    key: 4,
+    value: 'iPad Air(5세대)',
+  },
+  {
+    key: 5,
+    value: 'iPad mini(6세대)',
+  },
+];
+export const DropDownMore = [
+  {
+    key: 0,
+    value: 'iPad Pro 12.9(5세대)',
+  },
+  {
+    key: 1,
+    value: 'iPad Pro 12.9(4세대)',
+  },
+  {
+    key: 2,
+    value: 'iPad Pro 12.9(3세대)',
+  },
+  {
+    key: 3,
+    value: 'iPad Pro 12.9(2세대)',
+  },
+  {
+    key: 4,
+    value: 'iPad Pro 12.9(1세대)',
+  },
+  {
+    key: 5,
+    value: 'iPad Pro 11(3세대)',
+  },
+  {
+    key: 6,
+    value: 'iPad Pro 11(2세대)',
+  },
+  {
+    key: 7,
+    value: 'iPad Pro 11(1세대)',
+  },
+  {
+    key: 8,
+    value: 'iPad Pro 10.5',
+  },
+  {
+    key: 9,
+    value: 'iPad Pro 9.7',
+  },
+  {
+    key: 10,
+    value: 'iPad Air(4세대)',
+  },
+  {
+    key: 11,
+    value: 'iPad Air(3세대)',
+  },
+  {
+    key: 12,
+    value: 'iPad Air 2',
+  },
+  {
+    key: 13,
+    value: 'iPad Air (1세대)',
+  },
+  {
+    key: 14,
+    value: 'iPad(8세대)',
+  },
+  {
+    key: 15,
+    value: 'iPad(7세대)',
+  },
+  {
+    key: 16,
+    value: 'iPad(6세대)',
+  },
+  {
+    key: 17,
+    value: 'iPad(5세대)',
+  },
+  {
+    key: 18,
+    value: 'iPad mini(5세대)',
+  },
+  {
+    key: 19,
+    value: 'iPad mini4',
+  },
+  {
+    key: 20,
+    value: 'iPad mini3',
+  },
+  {
+    key: 11,
+    value: 'iPad mini2',
+  },
+];
