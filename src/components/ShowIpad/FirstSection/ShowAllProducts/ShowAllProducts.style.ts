1d835749594de2f6351824ded8dd9cd8a68b936c
seoAreum <1971236@hansung.ac.kr>
2023-11-24 05:26:14 +0900

    feat: 상품 카드를 보여주는 컴포넌트 1차 퍼블리싱

---
+++ src/components/ShowIpad/FirstSection/ShowAllProducts/ShowAllProducts.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ShowAllProductsContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProductsContainer = styled.div`
+  display: flex;
+
+  gap: 2.2rem;
+`;
