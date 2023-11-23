87d4fd0ddf80d924bbff5c65435653cd2af4f0c3
seoAreum <1971236@hansung.ac.kr>
2023-11-24 06:08:13 +0900

    chore: asset 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -5,29 +5,38 @@ import AddIcFold from './add_ic_fold.svg?react';
 import AddIcHandtap from './add_ic_handtap.svg?react';
 import AddIcUsbC from './add_ic_usb_c.svg?react';
 import AddIpadproIcClose from './add_ipadpro_ic_close.svg?react';
+
 import EllipseDark from './ellipse_dark.svg?react';
 import EllipseLight from './ellipse_light.svg?react';
+
 import GnbIcApple from './gnb_ic_apple.svg?react';
 import GnbIcSearch from './gnb_ic_search.svg?react';
 import GnbIcShop from './gnb_ic_shop.svg?react';
+
 import IcRightBold from './ic_right_bold.svg?react';
+
 import IcSwatch from './ic_swatch.svg?react';
 import IcSwatchSilver from './ic_swtach_silver.svg?react';
 import IcSwatchSpaceGray from './ic_swtach_spacegray.svg?react';
+
 import IcSwatchSmBeige from './ic_swatch_sm_beige.svg?react';
 import IcSwatchSmCoral from './ic_swatch_sm_coral.svg?react';
+import IcSwatchSmDarkgray from './ic_swatch_sm_darkgray.svg?react';
 import IcSwatchSmLightsilver from './ic_swatch_sm_lightsilver.svg?react';
 import IcSwatchSmNavy from './ic_swatch_sm_navy.svg?react';
 import IcSwatchSmPink from './ic_swatch_sm_pink.svg?react';
 import IcSwatchSmPurple from './ic_swatch_sm_purple.svg?react';
+import IcSwatchSmRed from './ic_swatch_sm_red.svg?react';
 import IcSwatchSmSilver from './ic_swatch_sm_silver.svg?react';
 import IcSwatchSmSkyblue from './ic_swatch_sm_skyblue.svg?react';
 import IcSwatchSmSpacegray from './ic_swatch_sm_spacegray.svg?react';
 import IcSwatchSmTurquiose from './ic_swatch_sm_turquiose.svg?react';
 import IcSwatchSmYellow from './ic_swatch_sm_yellow.svg?react';
+
 import IpadImgSwPattern01 from './ipad_img_sw_pattern01.svg?react';
 import IpadImgSwPattern02 from './ipad_img_sw_pattern02.svg?react';
 import IpadImgSwPattern03 from './ipad_img_sw_pattern03.svg?react';
+
 import IpadproIcApplebag from './ipadpro_ic_applebag.svg?react';
 import IpadproIcDownBlue24 from './ipadpro_ic_down_blue_24.svg?react';
 import IpadproIcDownGray24 from './ipadpro_ic_down_gray_24.svg?react';
@@ -48,6 +57,7 @@ import IpadproIcScrabNormal from './ipadpro_ic_scrab_normal.svg?react';
 import IpadproIcTruck from './ipadpro_ic_truck.svg?react';
 import IpadproIcUpGray32 from './ipadpro_ic_up_gray_32.svg?react';
 import IpadproIcUpGray from './ipadpro_ic_up_gray.svg?react';
+
 import MainIcBoxtruck from './main_ic_boxtruck.svg?react';
 import MainIcCard from './main_ic_card.svg?react';
 import MainIcPinstore from './main_ic_pinstore.svg?react';
@@ -60,29 +70,38 @@ export { AddIcFold };
 export { AddIcHandtap };
 export { AddIcUsbC };
 export { AddIpadproIcClose };
+
 export { EllipseDark };
 export { EllipseLight };
+
 export { GnbIcApple };
 export { GnbIcSearch };
 export { GnbIcShop };
+
 export { IcRightBold };
+
 export { IcSwatch };
 export { IcSwatchSilver };
 export { IcSwatchSpaceGray };
+
 export { IcSwatchSmBeige };
 export { IcSwatchSmCoral };
+export { IcSwatchSmDarkgray };
 export { IcSwatchSmLightsilver };
 export { IcSwatchSmNavy };
 export { IcSwatchSmPink };
 export { IcSwatchSmPurple };
+export { IcSwatchSmRed };
 export { IcSwatchSmSilver };
 export { IcSwatchSmSkyblue };
 export { IcSwatchSmSpacegray };
 export { IcSwatchSmTurquiose };
 export { IcSwatchSmYellow };
+
 export { IpadImgSwPattern01 };
 export { IpadImgSwPattern02 };
 export { IpadImgSwPattern03 };
+
 export { IpadproIcApplebag };
 export { IpadproIcDownBlue24 };
 export { IpadproIcDownGray24 };
@@ -103,6 +122,7 @@ export { IpadproIcScrabNormal };
 export { IpadproIcTruck };
 export { IpadproIcUpGray32 };
 export { IpadproIcUpGray };
+
 export { MainIcBoxtruck };
 export { MainIcCard };
 export { MainIcPinstore };
