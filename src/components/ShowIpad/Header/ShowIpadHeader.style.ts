850d818a999cd52b0cae37501d59582c268f13b2
seoAreum <1971236@hansung.ac.kr>
2023-11-24 03:41:52 +0900

    fix: 폰트 적용 안되는 이슈 해결

--- src/components/ShowIpad/Header/ShowIpadHeader.style.ts
+++ src/components/ShowIpad/Header/ShowIpadHeader.style.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
   display: flex;
-  ${({ theme: { fonts } }) => fonts.subheading1_1};
 
   margin-bottom: 2rem;
 
@@ -11,8 +10,10 @@ export const HeaderContainer = styled.header`
 
 export const Title = styled.p`
   color: ${({ theme: { colors } }) => colors.grayScale.gray8};
+  ${({ theme: { fonts } }) => fonts.subheading1_1};
 `;
 
 export const SubTitle = styled.p`
   color: ${({ theme: { colors } }) => colors.grayScale.gray6};
+  ${({ theme: { fonts } }) => fonts.subheading1_1};
 `;
