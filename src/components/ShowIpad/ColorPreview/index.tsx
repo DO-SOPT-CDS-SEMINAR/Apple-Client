9eed54267389a7faa502ba0050432a6606c97539
seoAreum <1971236@hansung.ac.kr>
2023-11-25 02:41:19 +0900

    refactor: 색상 미리보기 아이콘 띄워주는 코드 리팩토링

--- src/components/ShowIpad/ColorPreview/index.tsx
+++ src/components/ShowIpad/ColorPreview/index.tsx
@@ -1,102 +1,14 @@
-import {
-  IcSwatchSmBeige,
-  IcSwatchSmCoral,
-  IcSwatchSmDarkgray,
-  IcSwatchSmNavy,
-  IcSwatchSmPink,
-  IcSwatchSmPurple,
-  IcSwatchSmRed,
-  IcSwatchSmSilver,
-  IcSwatchSmSpacegray,
-  IcSwatchSmTurquiose,
-  IcSwatchSmYellow,
-  IpadImgSwPattern01,
-  IpadImgSwPattern02,
-  IpadImgSwPattern03,
-} from '../../../assets/icon';
-import { IcSwatchSmLightsilver } from '../../../assets/icon';
-
+import { COLOR_PREVIEW_LIST } from './ColorPreviewList';
 interface ColorPreviewProps {
   idx: number | string;
 }
 
 const ColorPreview = ({ idx }: ColorPreviewProps) => {
-  switch (idx) {
-    case 1:
-      return (
-        <>
-          <IcSwatchSmSilver />
-          <IcSwatchSmLightsilver />
-        </>
-      );
-
-    case 2:
-      return (
-        <>
-          <IcSwatchSmDarkgray />
-          <IcSwatchSmTurquiose />
-          <IcSwatchSmPink />
-          <IcSwatchSmPurple />
-          <IcSwatchSmBeige />
-        </>
-      );
-
-    case 3:
-      return (
-        <>
-          <IcSwatchSmNavy />
-          <IcSwatchSmCoral />
-          <IcSwatchSmYellow />
-          <IcSwatchSmLightsilver />
-        </>
-      );
-
-    case 4:
-      return (
-        <>
-          <IcSwatchSmDarkgray />
-          <IcSwatchSmLightsilver />
-        </>
-      );
-
-    case 5:
-      return (
-        <>
-          <IcSwatchSmSilver />
-          <IcSwatchSmLightsilver />
-        </>
-      );
-
-    case 'acc_1':
-      return (
-        <>
-          <IcSwatchSmNavy />
-          <IcSwatchSmRed />
-          <IcSwatchSmLightsilver />
-          <IcSwatchSmYellow />
-        </>
-      );
-
-    case 'acc_3':
-      return (
-        <>
-          <IpadImgSwPattern01 />
-          <IpadImgSwPattern02 />
-          <IpadImgSwPattern03 />
-        </>
-      );
-
-    case 'acc_4':
-      return (
-        <>
-          <IcSwatchSmLightsilver />
-          <IcSwatchSmSpacegray />
-        </>
-      );
-
-    default:
-      break;
-  }
+  return COLOR_PREVIEW_LIST.map((color) => {
+    if (idx === color.key) {
+      return color.value;
+    }
+  });
 };
 
 export default ColorPreview;
