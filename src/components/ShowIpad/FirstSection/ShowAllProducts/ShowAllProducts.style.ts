71bc8a3410cfa97f4ceb750d1d832ce2ab068878
seoAreum <1971236@hansung.ac.kr>
2023-11-24 21:07:24 +0900

    chore: position relative 적용해주는 위치 변경

--- src/components/ShowIpad/FirstSection/ShowAllProducts/ShowAllProducts.style.ts
+++ src/components/ShowIpad/FirstSection/ShowAllProducts/ShowAllProducts.style.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const ShowAllProductsContainer = styled.section`
   display: flex;
   flex-direction: column;
-  position: relative;
 `;
 
 export const ProductsContainer = styled.div`
   display: flex;
+  position: relative;
 
   gap: 2.2rem;
 `;
