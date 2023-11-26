0ffeb5963bd70ad4c7f7d8189a5209db1ed49379
Minseo Kang <rachel5640@naver.com>
2023-11-26 23:40:59 +0900

    feat. listCategory Wrapper Dummy Data 입력

---
+++ src/common/Nb/index 2.tsx
@@ -0,0 +1,51 @@
+import * as S from './Nb.style';
+import { useState } from 'react';
+
+export interface NbButtonProps {
+  label: string;
+  index: number;
+  onClick: (index: number) => void;
+  isActive: boolean;
+}
+
+export interface NbProps {
+  DATA: Array<string>;
+  isTab?: boolean;
+}
+
+//버튼 안에 라벨 넣기
+export const NbButton = ({ label, index, onClick, isActive }: NbButtonProps) => {
+  return (
+    <S.NbBtn onClick={() => onClick(index)} $isActive={isActive}>
+      {label}
+    </S.NbBtn>
+  );
+};
+
+const Nb = ({ DATA, isTab }: NbProps) => {
+  const [isClicked, setIsClicked] = useState(0);
+  const [, setIsActive] = useState(false);
+
+  // 화면 내 키 전송을 통한 이동용
+  const moveToPage = (index: number) => {
+    setIsClicked(index);
+    setIsActive(true);
+  };
+
+  // map을 통해서 버튼 생성
+  return (
+    <S.NbWrapper $isTab={isTab}>
+      {DATA.map((button, index) => (
+        <NbButton
+          key={index}
+          label={button}
+          index={index}
+          onClick={moveToPage}
+          isActive={isClicked === index}
+        />
+      ))}
+    </S.NbWrapper>
+  );
+};
+
+export default Nb;
