1b3a6e557a24b99fa53b30da0ac10319a2142ead
Minseo Kang <rachel5640@naver.com>
2023-11-26 22:45:13 +0900

    init: Main page 생성

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import Gnb from '../../common/Gnb';
+import Nb from '../../common/Nb';
+
+const Main = () => {
+  return (
+    <>
+      <Gnb />
+      <Nb />
+    </>
+  );
+};
+
+export default Main;
