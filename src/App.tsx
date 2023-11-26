34914a69bf02ca811d48493af89161c4effbbc2b
Minseo Kang <rachel5640@naver.com>
2023-11-27 07:03:02 +0900

    remove: 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import './styles/fonts/fonts.css';
-import ToggleMenu from './components/AddAccessory/toggle';
 
 function App() {
   console.log('초기세팅 완료');
@@ -12,7 +11,6 @@ function App() {
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <ToggleMenu />
       </ThemeProvider>
     </RecoilRoot>
   );
