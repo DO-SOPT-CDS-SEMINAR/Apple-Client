35042d99f04c34fe72e65c13121ba344e58b8595
seoAreum <1971236@hansung.ac.kr>
2023-11-28 05:05:57 +0900

    style: 캐러셀 적용되는 부분 스타일링 추가

--- src/components/ShowIpad/CommonSwiper/CommonSwiper.style.ts
+++ src/components/ShowIpad/CommonSwiper/CommonSwiper.style.ts
@@ -4,12 +4,19 @@ export const ProductsContainer = styled.div`
   display: flex;
   position: relative;
 
-  height: 55rem;
+  height: fit-content;
 
   .swiper {
     display: flex;
+    justify-content: left;
     scroll-behavior: smooth;
   }
+
+  .swiper-wrapper {
+    width: 500rem;
+    background-color: transparent;
+  }
+
   .swiper-slide {
     width: fit-content;
     background-color: transparent;
