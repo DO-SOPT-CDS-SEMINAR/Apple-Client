9fed584fe7277755fd4653b53922216e2a62eb99
seoAreum <1971236@hansung.ac.kr>
2023-11-28 05:06:28 +0900

    feat: 2, 3, 4, 7번 째 섹션을 호출하는 코드 추가

--- src/pages/ShowIpad/index.tsx
+++ src/pages/ShowIpad/index.tsx
@@ -1,10 +1,15 @@
 import * as S from './ShowIpad.style';
+import ShowCommonSection from '../../components/ShowIpad/CommonSection/ShowCommonSection';
 import ShowFirstSection from '../../components/ShowIpad/FirstSection/ShowFirstSection';
 
 const ShowIpadPage = () => {
   return (
     <S.ShowIpadPageContainer>
       <ShowFirstSection />
+      <ShowCommonSection section={2} />
+      <ShowCommonSection section={3} />
+      <ShowCommonSection section={4} />
+      <ShowCommonSection section={7} />
     </S.ShowIpadPageContainer>
   );
 };
