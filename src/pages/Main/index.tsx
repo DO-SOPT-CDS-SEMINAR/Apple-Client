e0e0b5039ee66e46069509b7ae598c566f26df6b
Minseo Kang <rachel5640@naver.com>
2023-11-28 19:07:13 +0900

    feat. 컴포넌트 합치기

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,19 @@
+import Gnb from '../../common/Gnb';
+import { GNB_CONTENTS } from '../../constant/gnbContents';
+import { MAIN_BUTTON_DATA } from '../../constant/mainButton';
+import Nb from '../../common/Nb';
+import Category from '../../components/Category';
+import TitleHeader from '../../components/Main/TitleHeader';
+
 const MainPage = () => {
-  return <div>메인페이지</div>;
+  return (
+    <div>
+      <Gnb DATA={GNB_CONTENTS} />
+      <TitleHeader />
+      <Category />
+      <Nb DATA={MAIN_BUTTON_DATA} />
+    </div>
+  );
 };
 
 export default MainPage;
