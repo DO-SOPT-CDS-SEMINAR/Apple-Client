56a3678cc4f2f1f05a62cddaab1cccbe3a4aae60
Minseo Kang <rachel5640@naver.com>
2023-11-29 07:22:56 +0900

    remove: 불필요한 코드 삭제

--- src/common/HoveredProduct/HoveredProduct.style.ts
+++ src/common/HoveredProduct/HoveredProduct.style.ts
@@ -11,7 +11,6 @@ export const ProductContainer = styled.div<ProductContainerProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 3;
   position: absolute;
 
   width: ${({ $width }) => css`
