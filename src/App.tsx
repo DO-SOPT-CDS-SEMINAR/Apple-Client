b7a06a7ecb5a000411f5e280e2342011c5761d93
Minseo Kang <rachel5640@naver.com>
2023-11-28 20:54:54 +0900

    feat: header 공통컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import './styles/fonts/fonts.css';
-import MainPage from './pages/Main';
 
 function App() {
   console.log('초기세팅 완료');
@@ -12,7 +11,6 @@ function App() {
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <MainPage />
       </ThemeProvider>
     </RecoilRoot>
   );
---
+++ src/components/Main/SectionHeader/SectionHeader.style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+
+  margin-bottom: 1rem;
+
+  ${({ theme: { fonts } }) => fonts.subheading1_1};
+
+  gap: 0.2rem;
+`;
+
+export const Title = styled.p`
+  color: ${({ theme: { colors } }) => colors.pointColor.red0};
+`;
+
+export const SubTitle = styled.p`
+  color: ${({ theme: { colors } }) => colors.grayScale.gray6};
+`;
