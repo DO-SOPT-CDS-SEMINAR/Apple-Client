eb5ea73977a0c0cc046908c836b01208dfc9358a
seoAreum <1971236@hansung.ac.kr>
2023-11-28 15:19:13 +0900

    init: 6번째 섹션 초기 구조

---
+++ src/components/ShowIpad/SixthSection/SixthCard/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './SixthCard.style';
+
+const SixthCard = () => {
+  return (
+    <>
+      <S.Test>SixthCard</S.Test>
+      <S.Test>SixthCard</S.Test>
+    </>
+  );
+};
+
+export default SixthCard;
