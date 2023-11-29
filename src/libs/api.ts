b6d7167be3699a1a4e77f110b4206f74302ecf0b
seoAreum <1971236@hansung.ac.kr>
2023-11-30 03:44:57 +0900

    chore: export 추가

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
+
+export default API;
