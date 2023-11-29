2e8ea5d310e98631645797c1a69a843dff783764
seoAreum <1971236@hansung.ac.kr>
2023-11-30 06:39:06 +0900

    feat: 상수 파일로 분리

---
+++ src/constant/ipadAdditionalInfo.ts
@@ -0,0 +1,8 @@
+export const ADDITIONAL_INFO = [
+  { id: 'acc_0' },
+  { id: 'acc_1' },
+  { id: 'acc_2', productTag: 'New' },
+  { id: 'acc_3' },
+  { id: 'acc_4' },
+  { id: 'acc_5', productTag: '무료 각인' },
+];
