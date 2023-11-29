87b9a8c0d896853ead3f27a401111fe56d8dc53c
seoAreum <1971236@hansung.ac.kr>
2023-11-30 07:29:47 +0900

    feat: 상수 데이터 추가

--- src/constant/ipadAdditionalInfo.ts
+++ src/constant/ipadAdditionalInfo.ts
@@ -6,3 +6,27 @@ export const ADDITIONAL_INFO = [
   { id: 'acc_4' },
   { id: 'acc_5', productTag: '무료 각인' },
 ];
+
+export const SIXTH_SECTION_ADD_INFO = [
+  {
+    id: 1,
+    subTitle: '빠른 시작 설정',
+  },
+  {
+    id: 2,
+    subTitle: '캠퍼스에서 만나는 Apple',
+  },
+  {
+    id: 3,
+    subTitle: 'Apple at Work',
+  },
+  {
+    id: 4,
+    subTitle: '데이터 옮기는 일도 간편하게',
+  },
+
+  {
+    id: 5,
+    subTitle: '보고 배우기',
+  },
+];
