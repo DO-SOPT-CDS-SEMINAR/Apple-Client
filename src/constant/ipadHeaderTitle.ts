ff2ba74999c2b66b10add1a0094901867c8d8ea4
seoAreum <1971236@hansung.ac.kr>
2023-11-30 06:01:24 +0900

    feat: 상수 파일로 분리

---
+++ src/constant/ipadHeaderTitle.ts
@@ -0,0 +1,27 @@
+export const IPAD_COMMON_HEADER_TITLE = [
+  {
+    section: 2,
+    title: '쇼핑 안내.',
+    subtitle: '결정을 못하겠다면 여기에서.',
+  },
+  {
+    section: 3,
+    title: '각종 할인 방법.',
+    subtitle: '당신에게 가장 알맞은 방법 찾아보기.',
+  },
+  {
+    section: 4,
+    title: '남다른 Apple Store.',
+    subtitle: '이곳에서 쇼핑해야 하는 더욱더 많은 이유.',
+  },
+  {
+    section: 7,
+    title: 'iPad 경험.',
+    subtitle: 'Apple의 모든 것과 연결되어 있는 느낌.',
+  },
+  {
+    section: 8,
+    title: '특별 할인.',
+    subtitle: '특별한 가격 혜택을 누리세요.',
+  },
+];
