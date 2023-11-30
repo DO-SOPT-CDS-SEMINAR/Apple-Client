8102f6e5d4a8f6c2536d2c34284802c1b9ce3b36
seoAreum <1971236@hansung.ac.kr>
2023-12-01 03:28:51 +0900

    feat: 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import './styles/fonts/fonts.css';
+import Router from './Router';
 
 function App() {
-  console.log('초기세팅 완료');
-
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Router />
       </ThemeProvider>
     </RecoilRoot>
   );
