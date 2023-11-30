4aefa79331841a60729a7bae2b5f2e6be4a1a387
seoAreum <1971236@hansung.ac.kr>
2023-11-30 22:44:49 +0900

    feat: 상수 데이터 파일로 분리

---
+++ src/constant/IpadProEvent.tsx
@@ -0,0 +1,14 @@
+export const IPAD_PRO_EVENT_CONTENTS = [
+  {
+    // exImg: <ImgIpadProAppleTvEx /> ,
+    // icImg: <ImgIpadProAppleTvEx />,
+    name: 'Apple TV+',
+    description: ['당신의 모든 기기에서 Apple Original 시리즈와', '영화를 감상하세요.**'],
+  },
+  {
+    // exImg: <ImgIpadProAppleAcEx /> ,
+    // icImg: <ImgIpadProAppleAcEx />,
+    name: 'Apple Arcade',
+    description: ['200개 이상의 게임을 광고 방해 없이. 게다가 끊임없이', '추가되는 새로운 게임.'],
+  },
+];
