f1546b774ef539eada0b8ef2c0154a80591eb951
seoAreum <1971236@hansung.ac.kr>
2023-11-30 12:49:36 +0900

    feat: 모든 섹션에서 헤더를 공통으로 받아오도록 수정

--- src/constant/ipadHeaderTitle.ts
+++ src/constant/ipadHeaderTitle.ts
@@ -1,5 +1,10 @@
 export const IPAD_COMMON_HEADER_TITLE = [
   {
+    section: 1,
+    title: '모든 모델.',
+    subtitle: '당신의 선택은?',
+  },
+  {
     section: 2,
     title: '쇼핑 안내.',
     subtitle: '결정을 못하겠다면 여기에서.',
@@ -13,6 +18,16 @@ export const IPAD_COMMON_HEADER_TITLE = [
     section: 4,
     title: '남다른 Apple Store.',
     subtitle: '이곳에서 쇼핑해야 하는 더욱더 많은 이유.',
+  },
+  {
+    section: 5,
+    title: 'IPad 액세서리.',
+    subtitle: '일도, 취미도 손쉽게.',
+  },
+  {
+    section: 6,
+    title: '설명 및 지원.',
+    subtitle: '차근차근 스페셜리스트의 도움과 함께.',
   },
   {
     section: 7,
