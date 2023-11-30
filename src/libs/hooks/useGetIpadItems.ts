6b4eb55b50cbff50f34175912edf54b5b45f0d69
seoAreum <1971236@hansung.ac.kr>
2023-11-30 12:53:22 +0900

    feat: api 통신하는 부분 커스텀 훅으로 분리

---
+++ src/libs/hooks/useGetIpadItems.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import API from '../api';
+import { AxiosError } from 'axios';
+
+export interface IpadItemProps {
+  productAsset: number;
+  productName: string;
+  productCost: string | null;
+  productImgUrl: string;
+}
+
+const useGetIpadItems = () => {
+  const [res, setRes] = useState<Array<IpadItemProps>>();
+
+  const getIpadItems = async () => {
+    try {
+      const res = await API.get(`/ipad/items`);
+      setRes(res.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getIpadItems();
+  }, []);
+
+  return { res };
+};
+
+export default useGetIpadItems;
