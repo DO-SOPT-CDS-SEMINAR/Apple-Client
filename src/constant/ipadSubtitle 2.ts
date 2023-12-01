2d8269880bdc7374c212c25ae2a3390178b17a21
Minseo Kang <111034927+rachel5640@users.noreply.github.com>
2023-12-01 14:39:04 +0900

    merge feature common to develop (#81)
    * style: 안 쓰는 스타일 속성 삭제
    
    * feat: 호버 공통 컴포넌트 초기 구현
    
    * feat: 공통으로 사용할 수 있게 width, height, border-radius 값을 props로 받아옴
    
    * feat: '제품 자세히 살펴보기' 버튼 동적으로 생성되도록 구현
    
    * [Common] Button 7가지 종류 공통 컴포넌트 구현 (#16)
    
    * chore: eslint 설정 관련 오류 해결
    
    * feat: default Button 구현 완료
    
    * feat: BuyButton 파일 생성
    
    * feat: BuyButton 구현
    
    * feat: DiplayButton 구현 및 모든 컴포넌트 style  단위 rem으로 변경
    
    * feat: giga button 구현 완료
    
    * feat: 장바구니에 담기 버튼 생성
    
    * feat: plus icon 버튼 icon 뺴고 구현
    
    * feat: 색상 선택 버튼 icon 제외하고 구현
    
    * feat: 색상 선택 버튼 색상 넘길 수 있도록 설정
    
    * feat: plus button 아이콘 적용 후 hover 효과 적용
    
    * style: buy button width 삭제
    
    * style: app 파일 초기화
    
    * refactor: 돈에 콤마 추가하는 법 좀 더 쉬운 방법으로 변환
    
    * style: theme 불러오는 코드 좀 더 간편하게 수정
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>
    
    * [Common] Dropdown component 구현 (#19)
    
    * feat: Dropdown component 생성
    
    * feat: dropdown icon 추가
    
    * style: app 파일 초기화
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>
    
    * [Common] page controller 공통컴포넌트 구현 (#21)
    
    * feat: PageController 폴더 및 파일 생성
    
    * chore: eslint관련 설정 수정
    
    * style: 페이지 tsx로 수정 및 구조 잡기 및 style 구현
    
    * feat: page controller page 상황에 맞게 달라질 수 있도록 icon 적용
    
    * style: app파일 초기화
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>
    
    * [Common]상단 Gnb, Banner, Nb 공통 컴포넌트 구현 (#15)
    
    * feat. Gnb 완성 및 sytle 적용
    
    * feat. Nb 구조 완성 및 style 적용
    
    * feat. 클릭시 active로 상태 변경 및 스타일 변경
    
    * style. svg파일 불러와서 변경
    
    * feat: nb 완성 및 DATA 따라 시 버튼 생성
    
    * remove: App에서 불필요한 import 삭제
    
    * feat. props 네이밍 수정
    
    * remove: 안쓰이는 isActive 함수 삭제
    
    * fix: 조건부 스타일링 이슈해결
    
    * fix: svg 파일 수정
    
    * feat: isTab 조건부 스타일링 추가
    
    * refactor: Gnb map 함수로 변경
    
    * [Common]1,2 뷰에 들어가는 Footer 완성 (#78)
    
    * feat: footer 구조 설계, data constant로 빼기
    
    * feat: 하단 정보 추가
    
    * feat: 상단에만 들어가는 header 추가
    
    * fix: 충돌 해결
    
    * fix: 충돌 해결
    
    ---------
    
    Co-authored-by: seoAreum <1971236@hansung.ac.kr>
    Co-authored-by: seoAreum <80264647+Arooming@users.noreply.github.com>
    Co-authored-by: Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

---
+++ src/constant/ipadSubtitle 2.ts
@@ -0,0 +1,17 @@
+export const IPAD_COMMON_SUB_TITLE = [
+  {
+    subtitle: ['모든 모델 비교하기', 'IPAD 스페셜리스트'],
+  },
+  {
+    subtitle: ['Apple Trade In', '편리한 결제 옵션', '인증 리퍼비쉬 제품'],
+  },
+  {
+    subtitle: ['무료 각인'],
+  },
+  {
+    subtitle: ['APPLE TV+', 'iPad를 선택하는 이유', 'IPADOS'],
+  },
+  {
+    subtitle: ['교육', '비지니스'],
+  },
+];
---
+++ src/pages/ShowIpad/ShowIpad.style 2.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ShowIpadPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  overflow-x: hidden;
+
+  margin-top: 4.2rem;
+  margin-left: 12rem;
+`;
