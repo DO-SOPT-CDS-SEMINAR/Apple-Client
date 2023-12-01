91bfa5675c7126a8c44a5293bb90c3357d1f93a9
seoAreum <1971236@hansung.ac.kr>
2023-12-01 19:58:58 +0900

    style: 피그마와 동일한 간격을 나타내도록 수정

--- src/common/Footer/CommonFooter/CommonFooter.style.ts
+++ src/common/Footer/CommonFooter/CommonFooter.style.ts
@@ -1,8 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ContentsProps {
+  $addMarginTop?: number;
+}
 
 export const CommonFooterContainer = styled.div`
   width: 98rem;
-  height: 50.9rem;
+  /* height: 50.9rem; */
   margin: 0 auto;
   padding: 1.7rem 0 1.1rem;
 `;
@@ -14,7 +18,9 @@ export const ContentsContainer = styled.div`
   gap: 1rem;
 `;
 
-export const Contents = styled.div`
+export const Contents = styled.div<ContentsProps>`
+  margin-top: ${({ $addMarginTop }) => ($addMarginTop === 5 || $addMarginTop === 10) && css`1rem`};
+
   color: ${({ theme: { colors } }) => colors.grayScale.gray6};
   ${({ theme: { fonts } }) => fonts.caption2};
 `;
--- src/common/Footer/CommonFooter/index.tsx
+++ src/common/Footer/CommonFooter/index.tsx
@@ -4,23 +4,47 @@ import * as S from './CommonFooter.style';
 const CommonFooter = () => {
   const { res } = useGetFooterContetnts();
   const topContents = res?.slice(0, 4);
-  const bottomContents = res?.slice(4, res.length);
+  const middleContents = res?.slice(3, 7);
+  const middle2Contents = res?.slice(7, 9);
+  const bottomContents = res?.slice(9, res.length - 1);
 
   return (
     <S.CommonFooterContainer>
-      <S.ContentsContainer>
-        {topContents?.map((data) => {
-          return <S.Contents key={data.id}>{data.content}</S.Contents>;
-        })}
-      </S.ContentsContainer>
-
-      {bottomContents?.map((data) => {
+      {topContents?.map((data) => {
         return (
           <S.ContentsContainer key={data.id}>
             <S.Contents>{data.content}</S.Contents>
           </S.ContentsContainer>
         );
       })}
+
+      <S.ContentsContainer>
+        {middleContents?.map((data) => {
+          return (
+            <S.Contents key={data.id} $addMarginTop={data.id}>
+              {data.content}
+            </S.Contents>
+          );
+        })}
+      </S.ContentsContainer>
+
+      {middle2Contents?.map((data) => {
+        return (
+          <S.ContentsContainer key={data.id}>
+            <S.Contents>{data.content}</S.Contents>
+          </S.ContentsContainer>
+        );
+      })}
+
+      <S.ContentsContainer>
+        {bottomContents?.map((data) => {
+          return (
+            <S.Contents key={data.id} $addMarginTop={data.id}>
+              {data.content}
+            </S.Contents>
+          );
+        })}
+      </S.ContentsContainer>
     </S.CommonFooterContainer>
   );
 };
