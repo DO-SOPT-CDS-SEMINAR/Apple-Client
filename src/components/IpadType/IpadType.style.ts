bb3aebb964ffde20a08309d24f5d6f90366e878c
Wang HoEun <hoeunwang@gmail.com>
2023-12-01 15:17:01 +0900

    feat: 컴포넌트 페이지 생성

---
+++ src/components/IpadType/IpadType.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 74.3rem;
+  height: 41.8rem;
+
+  border-radius: 1.8rem;
+`;
+
+export const Img = styled.img`
+  display: flex;
+`;
+
+export const Button = styled.button`
+  display: flex;
+`;
---
+++ src/components/IpadType/index.tsx
@@ -0,0 +1,15 @@
+import { EllipseLight } from '../../assets/icon';
+import * as S from './IpadType.style';
+
+const IpadType = () => {
+  return (
+    <S.Container>
+      <S.Img />
+      <S.Button>
+        <EllipseLight />
+      </S.Button>
+    </S.Container>
+  );
+};
+
+export default IpadType;
